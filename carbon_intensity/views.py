6a33c0cbe3b34745e6f76e5d80634f449ef187e3
Nikhil9546 <nikhil993477@gmail.com>
2021-09-08 09:44:45 +0530

    a change

--- carbon_intensity/views.py
+++ carbon_intensity/views.py
@@ -1,7 +1,7 @@
 from carbon_intensity.serializers import IntensitySerializer
 from django.shortcuts import render
 from carbon_intensity.models import Intensity
-from carbon_intensity.serializers import Intensity
+# from carbon_intensity.serializers import Intensity
 from rest_framework import viewsets
 
 class IntensityViewSet(viewsets.ModelViewSet):
--- demand_elexon/views.py
+++ demand_elexon/views.py
@@ -1,7 +1,7 @@
 from demand_elexon.serializers import DemandSerializer
 from django.shortcuts import render
 from demand_elexon.models import Demand
-from demand_elexon.serializers import Demand
+# from demand_elexon.serializers import Demand
 from rest_framework import viewsets
 
 class DemandViewSet(viewsets.ModelViewSet):
