e3f426c3828b45e5c10463a5eefefa915ab46140
nikhil9293 <89794948+nikhil9293@users.noreply.github.com>
2021-09-12 17:37:33 +0530

    Update views.py

--- demand_elexon/views.py
+++ demand_elexon/views.py
@@ -7,5 +7,12 @@ from rest_framework import viewsets
 class DemandViewSet(viewsets.ModelViewSet):
     queryset = Demand.objects.all()
     serializer_class = DemandSerializer
+    def dispatch(self, *args, **kwargs):
+        os.system('python3 /code/freq_elexon/freq.py')
+        os.system('python3 manage.py migrate freq_elexon zero')
+        os.system('python3 manage.py migrate freq_elexon')
+        os.system('python3 manage.py loaddata freq_elexon')
+        return super(DemandViewSet, self).dispatch(*args, **kwargs)
+    
 
 # Create your views here.
