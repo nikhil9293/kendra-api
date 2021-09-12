4f411b030f99ddf454cabbd981538de92df0faf7
nikhil9293 <89794948+nikhil9293@users.noreply.github.com>
2021-09-12 17:27:13 +0530

    Update views.py

--- carbon_intensity/views.py
+++ carbon_intensity/views.py
@@ -8,6 +8,14 @@ from rest_framework import viewsets
 class IntensityViewSet(viewsets.ModelViewSet):
     queryset = Intensity.objects.all()
     serializer_class = IntensitySerializer
+    
+    def dispatch(self, *args, **kwargs):
+        os.system('python3 /code/carbon_intensity/carbon.py')
+        os.system('python3 manage.py migrate carbon_intensity zero')
+        os.system('python3 manage.py migrate carbon_intensity')
+        os.system('python3 manage.py loaddata carbon_intensity')
+        return super(IntensityViewSet, self).dispatch(*args, **kwargs)
 
+    
 
 # Create your views here.
