a6baac5b95ce6126a08c02b9bfac5fae893d182c
nikhil9293 <89794948+nikhil9293@users.noreply.github.com>
2021-09-12 17:38:58 +0530

    Update views.py

--- demand_elexon/views.py
+++ demand_elexon/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from demand_elexon.models import Demand
 # from demand_elexon.serializers import Demand
 from rest_framework import viewsets
+import os
 
 class DemandViewSet(viewsets.ModelViewSet):
     queryset = Demand.objects.all()
